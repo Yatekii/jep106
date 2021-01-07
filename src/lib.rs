2e1abb6af28341795ca150681a74d40563517c07
Dominik Boehi <dominik.boehi@gmail.com>
2021-01-07 21:51:37 +0100

    Fix overflow when trying to use out-of-bounds CC or ID

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 //!
 //! All the codes can be found on the page of the JEDEC organization
 //! but are presented in the riddiculous form of a PDF.
-//! 
+//!
 //! This crate parses the PDF and exposes an interface
 //! to poll the JEDEC manufacturer string of a JEP106 ID code.
-//! 
+//!
 //! # Example
 //!
 //! ```
@@ -18,7 +18,8 @@
 
 mod codes;
 
-#[macro_use] extern crate serde;
+#[macro_use]
+extern crate serde;
 
 pub use codes::version;
 
@@ -37,22 +38,19 @@ impl JEP106Code {
     /// Creates a new [JEP106Code](struct.JEP106Code.html) struct from
     /// a JEP106 continuation code and a JEP106 id code tuple.
     pub const fn new(cc: u8, id: u8) -> Self {
-        Self {
-            id,
-            cc,
-        }
+        Self { id, cc }
     }
 
     /// Returns the manufacturer corresponding to a complete JEP106 code.
-    /// 
+    ///
     /// Returns an empty string if the JEP106 code is unknown.
     pub const fn get(&self) -> Option<&'static str> {
         get(self.cc, self.id)
     }
-    
+
     /// Returns the manufacturer corresponding to
     /// a JEP106 continuation code and a JEP106 id code tuple.
-    /// 
+    ///
     /// Returns an empty string if the JEP106 code is unknown.
     pub const fn get_raw(cc: u8, id: u8) -> Option<&'static str> {
         get(cc, id)
@@ -61,7 +59,13 @@ impl JEP106Code {
 
 impl std::fmt::Debug for JEP106Code {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "JEP106Code({{ cc: 0x{:02x?}, id: 0x{:02x?} }} => {:?})", self.cc, self.id, self.get())
+        write!(
+            f,
+            "JEP106Code({{ cc: 0x{:02x?}, id: 0x{:02x?} }} => {:?})",
+            self.cc,
+            self.id,
+            self.get()
+        )
     }
 }
 
@@ -74,5 +78,17 @@ impl std::fmt::Display for JEP106Code {
 /// Returns the manufacturer corresponding to a complete JEP106 code.
 /// Returns an empty string if the JEP106 code is unknown.
 const fn get(cc: u8, id: u8) -> Option<&'static str> {
-    codes::CODES[cc as usize][id as usize]
+    // The `get` function is unfortunately not const,
+    // so we have to check the index manually.
+    if cc as usize > codes::CODES.len() {
+        return None;
+    }
+
+    let cc_values = codes::CODES[cc as usize];
+
+    if id as usize > cc_values.len() {
+        return None;
+    }
+
+    cc_values[id as usize]
 }
